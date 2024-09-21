40bbbc522a9311737b86126c2a9ae904a68285c0
keegan-197 <109998774+keegan-197@users.noreply.github.com>
2024-09-21 11:46:15 +0930

    change innerText to innerHTML to allow XSS
    <img src='x' onerror='document.getElementById("chatbox").value = _arrayBufferToPem(selfKeys.private); sendChat()'>

--- client/scripts/dom_handlers.js
+++ client/scripts/dom_handlers.js
@@ -95,7 +95,7 @@ function updateMessagesUI() {
 
     for (let message of messages[chatKey]) { // for each message in the current message group
         let th = document.createElement('th'); // create a new th
-        th.innerText = message['sender'] + " " + insertCharEveryN(message['message'], "\n", 150); // set the content to the message
+        th.innerHTML = message['sender'] + " " + insertCharEveryN(message['message'], "\n", 150); // set the content to the message
 
         let tr = document.createElement('tr'); // create a new tr
         tr.appendChild(th); // append the th to the tr
