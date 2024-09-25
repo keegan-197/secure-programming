0d12a5a64efaf0d2d926d5fd05fd336d97579ead
keegan-197 <109998774+keegan-197@users.noreply.github.com>
2024-09-25 18:23:40 +0930

    update generateAESKey()

--- client/scripts/crypto.js
+++ client/scripts/crypto.js
@@ -1,12 +1,10 @@
 async function generateAESKey() {
     // generate an IV, AES key, and settings object
-    let new_iv = window.crypto.getRandomValues(new Uint8Array(16));
-
-    let aes_settings = { name: "AES-GCM", iv: new_iv, length: 256 };
+    let aes_settings = { name: "AES-GCM", iv: selfKeys["iv"], length: 256 };
 
     let generated_key = await window.crypto.subtle.generateKey(aes_settings, true, ["encrypt", "decrypt"]);
 
-    return [new_iv, generated_key, aes_settings]
+    return [generated_key, aes_settings]
 }
 
 async function sha256Digest(cdata) { // create a digest from a piece of data
